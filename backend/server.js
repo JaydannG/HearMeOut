5f32febf67b2f80bbb525b56a9021507eafd61d9
JaydannG <grossmanjaydann@gmail.com>
2025-09-12 12:48:47 -0400

    getting backend API requests working. can now make requests and print result of a random track by the given artist to the console

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 async function getSpotifyToken() {
     const response = await axios.post(
         "https://accounts.spotify.com/api/token",
-        new URLSearchParams({ grant_type: "client_credentials "}),
+        new URLSearchParams({ grant_type: "client_credentials" }),
         {
             headers: {
                 Authorization:
-                    "Basic" +
+                    "Basic " +
                     Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64"),
                     "Content-Type": "application/x-www-form-urlencoded",
             },
@@ -32,12 +32,29 @@ async function getSpotifyToken() {
 }
 
 // function to get the form post from the frontend
-app.post('/submit-form', (req, res) => {
-    const artist = req.body.artist;
-    console.log("Artist: ", artist);
-    res.json({ message: `Received artist: ${artist}`});
+app.post('/submit-form', async (req, res) => {
+    try {
+        const artist = req.body.artist;
+        const token = await getSpotifyToken();
+
+        const searchRes = await axios.get(`${BASE_URL}/search`, {
+            headers: { Authorization: `Bearer ${token}` },
+            params: { q: `artist:${artist}`, type: "track" },
+        });
+
+        const tracks = searchRes.data.tracks.items;
+        if (tracks.length === 0) {
+            return res.status(404).json({ error: "Could not find any tracks"});
+        }
+
+        const randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
+        console.log(randomTrack.name);
+    } catch (error) {
+        console.error("Error fetching from Spotify", error);
+        res.status(500).json({ error: "Error with Spotify API"});
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://loaclhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
